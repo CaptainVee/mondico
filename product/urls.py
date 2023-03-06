58fc89bc6292a3056351c900994c5b46ccd56c04
CaptainVee <captainvee3@gmail.com>
2023-03-06 21:24:32 +0100

    chore: fixed a url bug

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,7 @@ from .views import home, about, contact, product_item
 
 
 urlpatterns = [
-
+    path("", home, name="home"),
     path("about/", about, name="about"),
     path("contact/", contact, name="contact"),
     path("products/", product_item, name="product_item"),
