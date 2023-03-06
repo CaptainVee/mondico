68fd4751aefaba7e6616fc78509acd5e8c321e1b
CaptainVee <captainvee3@gmail.com>
2023-03-06 21:15:07 +0100

    added some couple of blank pages

--- product/views.py
+++ product/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "product/index.html")
+    return render(request, "product/home.html")
+
+def about(request):
+    return render(request, "product/about.html")
+
+def product_item(request):
+    return render(request, "product/product_item.html")
+    
+def contact(request):
+    return render(request, "product/contact.html")
